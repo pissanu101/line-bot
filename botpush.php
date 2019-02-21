cb2eb4bfad0fd5d1e6e73951b7ae6303d6002171
pissanu101 <47841394+pissanu101@users.noreply.github.com>
2019-02-21 12:29:20 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -6,9 +6,9 @@ require "vendor/autoload.php";
 
 $access_token = 'dkovOt+zHrAVARtFYkJbfBI9uIFEDHg5Dyv44WXmsYlqu+dULj2vf5snhyKl7d//0Kw8/p+GX/lJxuYoFKIV8zGUJZqdGkfLNPBV71md5nDBWlSjcEa0NFcbb5Bws4yirPqE/xaoV/hDow3XFlkRWgdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '041894f452a191dad4d7ecaf2441ba9f';
+$channelSecret = '9b51168ef28e112e5b2eaaf15a24346b';
 
-$pushID = 'U229da5b78f4a7deb7032e2b37c79e5d5';
+$pushID = 'U0c4dd7723b2b4e2702b023cd6adf1958';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
